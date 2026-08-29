ba4b605f2368803adc7ada1319818cea7107928f
Nicolas Piñera <nicolaspinera@gmail.com>
2026-08-29 16:16:43 -0300

    Agregado: Modulo ALU + Logica de TOP

--- tp1-arqui/tp1-arqui.srcs/sources_1/new/alu.v
+++ tp1-arqui/tp1-arqui.srcs/sources_1/new/alu.v
@@ -1,38 +1,59 @@
 // Solamente se encarga de obtener A - B - Opcode y generar el resultado final
-module alu_modulo #(parameter MSB = 8)(
-    input [MSB-1:0] i_a,
-    input [MSB-1:0] i_b,
-    input [5:0] i_opc,
-    output reg [MSB-1:0] o_resultado
+module alu #(parameter MSB = 8)(
+    // Salidas
+    output reg      [MSB-1:0]   o_resultado , // Registro de resultado
+    output reg                  o_zero      , // Flag de Zero
+    output reg                  o_overflow  , // Flag de Overflow
+    // Entradas
+    input signed    [MSB-1:0]   i_a     ,
+    input signed    [MSB-1:0]   i_b     ,
+    input           [5:0]       i_opc
     );
-    
+ 
     always@(*) begin
+    o_overflow = 1'b0;
     case(i_opc)
-        'b100000: begin 
+        // ADD
+        6'b100000: begin 
             o_resultado = i_a + i_b;
-        end // add
-        'b100010: begin 
+            // Si A y B tienen el mismo signo y el resultado tiene signo contrario, nos pasamos
+            if (i_a[MSB-1] == i_b[MSB-1] && o_resultado[MSB-1] != i_a[MSB-1])
+                o_overflow = 1'b1;
+        end
+        // SUB
+        6'b100010: begin 
             o_resultado = i_a - i_b;
-        end // sub
-        'b100100: begin 
-            o_resultado = i_a & i_b;
-        end // and
-        'b100101: begin 
-            o_resultado = i_a | i_b;
-        end // or
-        'b100110: begin 
-            o_resultado = {MSB{1'b0}};
-        end // xor
-        'b000011: begin 
-            o_resultado = {MSB{1'b0}};
-        end // sra
-        'b000010: begin 
-            o_resultado = {MSB{1'b0}};
-        end // srl
-        'b100111: begin 
-            o_resultado = {MSB{1'b0}};
-        end // nor
+            // Si A y B tienen signo contrario y el resultado tiene signo contrario a A, nos pasamos
+            if (i_a[MSB-1] != i_b[MSB-1] && o_resultado[MSB-1] != i_a[MSB-1])
+                o_overflow = 1'b1;
+        end
+        
+        // AND
+        6'b100100: o_resultado = i_a & i_b;
+        
+        // OR
+        6'b100101: o_resultado = i_a | i_b;
+        
+        // XOR
+        6'b100110: o_resultado = i_a ^ i_b;
+        
+        // SRA -> Desplazamiento aritmetico
+        6'b000011: o_resultado = i_a >>> i_b; // A es el valor y B la cant a desplazar
+        
+        // SRL -> Desplazamiento Logico
+        6'b000010: o_resultado = i_a >> i_b;
+        
+        // NOR
+        6'b100111: o_resultado = ~(i_a | i_b);
+        
+        // Pone a cero el resultado -> Leds apagados
         default: o_resultado = {MSB{1'b0}};
     endcase
+    
+        // Verificamos si el resultado fue 0
+        if(!o_resultado)
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
     end
 endmodule
--- tp1-arqui/tp1-arqui.srcs/sources_1/new/top.v
+++ tp1-arqui/tp1-arqui.srcs/sources_1/new/top.v
@@ -1,17 +1,43 @@
 // Aca se genera la maquina de estado
 module top
 #(parameter MSB = 8)(
-// salidas
-
-// entradas
-    input [MSB-1:0] i_A,
-    input [MSB-1:0] i_B,
-    input [5:0] i_opcode,
-    input i_reset,
-    input clock
+    // salidas
+    output [MSB-1:0] o_led,     // Salida de 8 bit para leds
+    output           o_zero,    // Flag de zero    
+    output           o_overflow,// Flag de overflow
+    
+    // entradas
+    input [2:0]     i_abc,      // Nos define el valor a ingresar
+    input [MSB-1:0] i_datos,    // Datos (A,B,opcode)    
+    input           i_reset,    // Señal de reset
+    input           clock       // Señal de clock
     );
+    reg signed [MSB-1:0] A,B;
+    reg [5:0] opcode;
     
     always@(posedge clock) begin
-        //            
+        // Reset sincrono al clock
+        if(i_reset)begin
+            A <= {MSB{1'b0}};
+            B <= {MSB{1'b0}};
+            opcode <= {6{1'b0}};
+        end
+        else begin
+            case(i_abc)
+                3'b001: A<=i_datos; // Obtenemos A
+                3'b010: B<=i_datos; // Obtenemos B
+                3'b100: opcode<=i_datos[5:0]; // Obtenemos el opcode
+                default:; // Todos mantienen su valor
+            endcase
+        end          
     end
+    
+    alu #(.MSB(MSB)) u_alu(
+        .o_resultado(o_led),
+        .o_zero(o_zero),
+        .o_overflow(o_overflow),
+        .i_a(A),
+        .i_b(B),
+        .i_opc(opcode)
+    );
 endmodule
